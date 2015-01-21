4792b584edd32a43355fa2743e96ccf2fef79044
Ben Calton <bac@pml.ac.uk>
2015-01-21 11:05:00 +0000

    Additional provider details

--- config/providers.py
+++ config/providers.py
@@ -10,5 +10,14 @@ providers ={
   }, 
   "TUWien": {
     "website": "http://geo.tuwien.ac.at/"
+  },
+  "UU": {
+    "website": "http://www.uu.nl/"
+  },
+  "UniKassel": {
+    "website": "http://www.uni-kassel.de/"
+  },
+  "CSIRO": {
+    "website": "http://www.csiro.au/"
   }
 }
