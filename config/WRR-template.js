2e0def10c3c13004c5fb35e2075209b7b72dfef9
Ben Calton <bac@pml.ac.uk>
2015-05-27 16:20:38 +0100

    Template for creating model outputs

---
+++ config/WRR-template.js
@@ -0,0 +1,455 @@
+         "Precip": {
+            "niceName": "Total Precipitation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Precipitation"
+            ]
+         },
+         "Evap": {
+            "niceName": "Total Evapotranspiration",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration"
+            ]
+         },
+         "Runoff": {
+            "niceName": "Total Runoff",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Surface Water"
+            ]
+         },
+         "Rainf": {
+            "niceName": "Rainfall",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Meteorology",
+            ]
+         },
+         "Qs": {
+            "niceName": "Total Surface Runoff",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Surface Water"
+            ]
+         },
+         "Qsb": {
+            "niceName": "Sub-Surface Runoff",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Sub-Surface Water"
+            ]
+         },
+         "Qrec": {
+            "niceName": "Recharge",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+            ]
+         },
+         "Qsm": {
+            "niceName": "Snowmelt",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Surface Water"
+            ]
+         },
+         "PotEvap": {
+            "niceName": "Potential Evapotranspiration",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration",
+            ]
+         },
+         "ECanop": {
+            "niceName": "Interception Evaporation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration"
+            ]
+         },
+         "TVeg": {
+            "niceName": "Vegetation Transpiration",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration"
+            ]
+         },
+         "ESoil": {
+            "niceName": "Bare Soil Evaporation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration"
+            ]
+         },
+         "EWater": {
+            "niceName": "Open Water Evaporation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Evapotranspiration"
+            ]
+         },
+         "RivOut": {
+            "niceName": "River Discharge",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Surface Water"
+            ]
+         },
+         "Dis": {
+            "niceName": "Point River Discharge",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Water Balance Components",
+               "Surface Water"
+            ]
+         },
+         "SWnet": {
+            "niceName": "Net Shortwave Radiation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Energy Balance Components",
+               "Radiation"
+            ]
+         },
+         "LWnet": {
+            "niceName": "Net Longwave Radiation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Energy Balance Components",
+               "Radiation"
+            ]
+         },
+         "Qle": {
+            "niceName": "Latent Heat Flux",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Energy Balance Components",
+            ]
+         },
+         "Qh": {
+            "niceName": "Sensible Heat Flux",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "Energy Balance Components",
+            ]
+         },
+         "AvgSurfT": {
+            "niceName": "Average Surface Temperature",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Meteorology"
+            ]
+         },
+         "Albedo": {
+            "niceName": "Surface Albedo",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+            ]
+         },
+         "LAI": {
+            "niceName": "Leaf Area Index",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+            ]
+         },
+         "SWE": {
+            "niceName": "Snow Water Equivalent",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Surface Water"
+            ]
+         },
+         "CanopInt": {
+            "niceName": "Total Canopy water Storage",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+            ]
+         },
+         "SWEVeg": {
+            "niceName": "SWE Intercepted by Vegetation",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+            ]
+         },
+         "SurfStor": {
+            "niceName": "Surface Water Storage",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Surface Water"
+            ]
+         },
+         "WaterTableD": {
+            "niceName": "Water Table Depth",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Sub-Surface Water"
+            ]
+         },
+         "SnowFrac": {
+            "niceName": "Snow Covered Fraction",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Surface Water"
+            ]
+         },
+         "SnowDepth": {
+            "niceName": "Snow Depth",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Surface Water"
+            ]
+         },
+         "SurfMoist": {
+            "niceName": "Surface Soil Moisture",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Surface Water"
+            ]
+         },
+         "RootMoist": {
+            "niceName": "Root Zone Soil Moisture",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Sub-Surface Water"
+            ]
+         },
+         "TotMoist": {
+            "niceName": "Total Soil Moisture",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Sub-Surface Water"
+            ]
+         },
+         "GroundMoist": {
+            "niceName": "Ground Water",
+            "data_provider": "ProviderValue",
+            "interval": "IntervalValue",            
+            "model": "ModelValue",
+            "data_type": "Ecosystem Model",
+            "forcing_version": "Version 0",
+            "region": "Global",
+            "indicator_type": [
+               "Water Resource Re-analysis v1",
+               "State Variables",
+               "Sub-Surface Water"
+            ]
+         }
